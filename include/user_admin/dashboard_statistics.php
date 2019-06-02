4bb8b48ec4c6c0b407dd01dc30e1f1a758eced8b
Mir Lutfur Rahman <mirlutfur.rahman@gmail.com>
2019-06-02 20:55:01 +0600

    Add files via upload

---
+++ include/user_admin/dashboard_statistics.php
@@ -0,0 +1,572 @@
+<!--section-1 large-screen -->
+<div class="w3-container w3-hide-small w3-hide-medium">
+	<div class="w3-container w3-right" style="width:82%;margin:30px 0px;">
+		<div class="w3-row">
+			<!-- col-1 -->
+			<div class="w3-half w3-padding">
+				<div class="w3-container w3-hover-shadow w3-round-large w3-cursor w3-border w3-border-dark-gray w3-hover-border-teal w3-topbar w3-bottombar" style="padding:15px 30px;">
+					<p class="w3-xxlarge w3-bold w3-bottombar w3-border-teal" style="margin: 5px 0px;">
+						Total Members
+					</p>
+					<p class="w3-xxlarge w3-text-blue" style="margin:8px 0px;">
+						<i class="fa fa-user"></i> 1000
+					</p>
+					<table class="w3-table w3-bold">
+						<tr>
+							<td>
+								CSE
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								BBA
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								LAW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+						<tr>
+							<td>
+								ENG
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								AS&SW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Other
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<!-- col-2 -->
+			<div class="w3-half w3-padding">
+				<div class="w3-container w3-hover-shadow w3-round-large w3-cursor w3-border w3-border-dark-gray w3-hover-border-teal w3-topbar w3-bottombar" style="padding:15px 30px;">
+					<p class="w3-xxlarge w3-bold w3-bottombar w3-border-teal" style="margin: 5px 0px;">
+						Total Books
+					</p>
+					<p class="w3-xxlarge w3-text-indigo" style="margin:8px 0px;">
+						<i class="fa fa-book"></i> 1000
+					</p>
+					<table class="w3-table w3-bold">
+						<tr>
+							<td>
+								CSE
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								BBA
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								LAW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+						<tr>
+							<td>
+								ENG
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								AS&SW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Other
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- section-1 medium-screen -->
+<div class="w3-container w3-hide-small w3-hide-large" style="padding:0px;">
+	<div class="w3-container w3-right" style="width:68%;margin:50px 0px;padding:0px;">
+		<div class="w3-row w3-padding-small">
+			<!-- col-1 -->
+			<div class="w3-half w3-padding-small">
+				<div class="w3-container w3-hover-shadow w3-round-large w3-cursor w3-border w3-border-dark-gray w3-hover-border-teal w3-topbar w3-bottombar" style="padding:5px 5px;">
+					<p class="w3-large w3-bold w3-bottombar w3-border-teal" style="margin: 5px 0px;">
+						Total Members
+					</p>
+					<p class="w3-large w3-text-blue" style="margin:8px 0px;">
+						<i class="fa fa-user"></i> 1000
+					</p>
+					<table class="w3-table w3-bold w3-small" style="width:100%;">
+						<tr>
+							<td>
+								CSE
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								BBA
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								LAW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+						<tr>
+							<td>
+								ENG
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								AS&SW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Other
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<!-- col-2 -->
+			<div class="w3-half w3-padding-small">
+				<div class="w3-container w3-hover-shadow w3-round-large w3-cursor w3-border w3-border-dark-gray w3-hover-border-teal w3-topbar w3-bottombar" style="padding:5px 5px;">
+					<p class="w3-large w3-bold w3-bottombar w3-border-teal" style="margin: 5px 0px;">
+						Total Books
+					</p>
+					<p class="w3-large w3-text-indigo" style="margin:8px 0px;">
+						<i class="fa fa-book"></i> 1000
+					</p>
+					<table class="w3-table w3-bold w3-small" style="width:100%;">
+						<tr>
+							<td>
+								CSE
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								BBA
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								LAW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+						<tr>
+							<td>
+								ENG
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								AS&SW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Other
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<!-- section-1 small-screen -->
+<div class="w3-container w3-hide-medium w3-hide-large" style="padding:0px;">
+	<div class="w3-container" style="width:100%;margin:260px 0px 20px 0px;padding:0px;">
+		<div class="w3-row w3-padding-small">
+			<!-- col-1 -->
+			<div class="w3-half w3-padding-small">
+				<div class="w3-container w3-hover-shadow w3-round-large w3-cursor w3-border w3-border-dark-gray w3-hover-border-teal w3-topbar w3-bottombar" style="padding:5px 5px;">
+					<p class="w3-xlarge w3-bold w3-bottombar w3-border-teal" style="margin: 5px 0px;">
+						Total Members
+					</p>
+					<p class="w3-xlarge w3-text-blue" style="margin:8px 0px;">
+						<i class="fa fa-user"></i> 1000
+					</p>
+					<table class="w3-table w3-bold w3-medium" style="width:100%;">
+						<tr>
+							<td>
+								CSE
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								BBA
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								LAW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+						<tr>
+							<td>
+								ENG
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								AS&SW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Other
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-blue" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<!-- col-2 -->
+			<div class="w3-half w3-padding-small">
+				<div class="w3-container w3-hover-shadow w3-round-large w3-cursor w3-border w3-border-dark-gray w3-hover-border-teal w3-topbar w3-bottombar" style="padding:5px 5px;">
+					<p class="w3-xlarge w3-bold w3-bottombar w3-border-teal" style="margin: 5px 0px;">
+						Total Books
+					</p>
+					<p class="w3-xlarge w3-text-indigo" style="margin:8px 0px;">
+						<i class="fa fa-book"></i> 1000
+					</p>
+					<table class="w3-table w3-bold w3-medium" style="width:100%;">
+						<tr>
+							<td>
+								CSE
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								BBA
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								LAW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+						<tr>
+							<td>
+								ENG
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:25%">25%</div>
+								</div>
+							</td>
+							<td>
+								250
+							</td>
+						</tr>
+						<tr>
+							<td>
+								AS&SW
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:15%">15%</div>
+								</div>
+							</td>
+							<td>
+								150
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Other
+							</td>
+							<td style="width:100%;">
+								<div class="w3-light-gray w3-round-large">
+									<div class="w3-container w3-round w3-indigo" style="width:10%">10%</div>
+								</div>
+							</td>
+							<td>
+								100
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- user_admin/dashboard.php
+++ user_admin/dashboard.php
@@ -8,6 +8,10 @@
 		?>
 		<div class="w3-content w3-white w3-center" style="max-width:2000px;margin-top:80px;min-height:320px;height:auto;">
 			
+			<?php
+				include("../include/user_admin/dashboard_statistics.php");
+			?>
+			
 		</div>
 
 <?php
